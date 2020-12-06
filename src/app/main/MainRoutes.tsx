d7aeca8d2f4544c2f4482de3ec81c95a4b37c4cc
Marko Arsic <marsicdev@gmail.com>
2020-12-06 18:48:33 +0100

    Add lazy loading routes

--- src/app/main/MainRoutes.tsx
+++ src/app/main/MainRoutes.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import UsersPage from 'app/users/UsersPage'
 import DashboardPage from 'app/dashboard/DashboardPage'
 
+const UsersPage = React.lazy(() => import('app/users/UsersPage'))
+
 const MainRoutes = () => (
-    <Switch>
-        <Route path="/users">
-            <UsersPage />
-        </Route>
-        <Route exact path="/">
-            <DashboardPage />
-        </Route>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+            <Route path="/users">
+                <UsersPage />
+            </Route>
+            <Route exact path="/">
+                <DashboardPage />
+            </Route>
+        </Switch>
+    </Suspense>
 )
 
 export default MainRoutes
